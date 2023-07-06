ec460d63fd071e4e87dd9b43aff1758405f02ec9
Gleidson Leite <gledson.leytte@hotmail.com>
2023-07-05 23:55:31 -0300

    list sum implemmented

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,7 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: list_length(list, 0)
+
+  defp list_length([], acc), do: acc
+
+  defp list_length([_ | tail], acc), do: list_length(tail, acc + 1)
 end
